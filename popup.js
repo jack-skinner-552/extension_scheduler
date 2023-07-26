7c3bb3b2794485b4e0f531322b4bc6ef0031f7de
Skinner <jskinn848@cable.comcast.com>
2023-07-26 14:01:42 -0600

    Added 'Extensions Enabled/Disabled' text to Popup

--- popup.js
+++ popup.js
@@ -1,30 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Code for displaying the extension icons in the popup
-  const extensionIconsContainer = document.getElementById('extensionIcons');
+  // Get the value of 'isWithinActiveTimeRange' from Chrome storage
+  chrome.storage.local.get('isWithinActiveTimeRange', function (data) {
+    const isWithinActiveTimeRange = data.isWithinActiveTimeRange;
 
-  chrome.management.getAll(function (extensions) {
-    extensions.sort(function (a, b) {
-      return a.name.localeCompare(b.name);
-    });
+    // Update the status text based on the value of 'isWithinActiveTimeRange'
+    const statusText = document.getElementById('status');
+    statusText.textContent = isWithinActiveTimeRange
+      ? 'Extensions Disabled:'
+      : 'Extensions Enabled:';
 
-    chrome.storage.local.get('checkedExtensions', function (data) {
-      const checkedExtensions = data.checkedExtensions || [];
+    // Code for displaying the extension icons in the popup
+    const extensionIconsContainer = document.getElementById('extensionIcons');
 
-      extensions.forEach(function (extension) {
-        if (extension.type === 'extension' && extension.id !== chrome.runtime.id) {
-          const icon = document.createElement('img');
-          icon.src = extension.icons ? extension.icons[0].url : 'icon-default.png';
-          icon.classList.add('extension-icon');
+    chrome.management.getAll(function (extensions) {
+      extensions.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+      });
 
-          // Show or hide the extension icon based on whether it is checked
-          if (checkedExtensions.includes(extension.id)) {
-            icon.style.display = 'block';
-          } else {
-            icon.style.display = 'none';
+      chrome.storage.local.get('checkedExtensions', function (data) {
+        const checkedExtensions = data.checkedExtensions || [];
+
+        extensions.forEach(function (extension) {
+          if (extension.type === 'extension' && extension.id !== chrome.runtime.id) {
+            const icon = document.createElement('img');
+            icon.src = extension.icons ? extension.icons[0].url : 'icon-default.png';
+            icon.classList.add('extension-icon');
+
+            // Show or hide the extension icon based on whether it is checked
+            if (checkedExtensions.includes(extension.id)) {
+              icon.style.display = 'block';
+            } else {
+              icon.style.display = 'none';
+            }
+
+            extensionIconsContainer.appendChild(icon);
           }
-
-          extensionIconsContainer.appendChild(icon);
-        }
+        });
       });
     });
   });
